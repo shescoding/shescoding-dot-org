ce0aa47f9159cfa1a960c079ea9902a00dfe11fc
Bri Dotson <bridotson@gmail.com>
2015-04-05 14:44:45 -0700

    add team page and styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+  def team
+    @team_members = 7.times.collect {"Nathalie Steinmetz"}
+    @last_row = if @team_members.length % 4 != 0
+        @team_members.length - (@team_members.length % 4)
+    end
+  end
 end
