755be10ecf641320ffe5074eb2bbd81516995f00
Bri Dotson <bridotson@gmail.com>
2015-04-06 20:42:17 -0700

    remove mock model, add tooltips for advisors, add upper image placeholder

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,4 @@
 class WelcomeController < ApplicationController
   def index
   end
-  def team
-    @team_members = 7.times.collect {"Nathalie Steinmetz"}
-    @last_row = if @team_members.length % 4 != 0
-        @team_members.length - (@team_members.length % 4)
-    end
-  end
 end
