44e841a316d6df0f997cd8d2f331b57a129b057c
Calla <callapatel@gmail.com>
2015-04-07 13:43:00 -0700

    content on page, spacing, title

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def about
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   resources :resources
 
+  get 'welcome/about',            to: "welcome#about"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
