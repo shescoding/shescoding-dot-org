3baee15fd619c577552db3dc72416ae947a88616
Kristina Hjertberg <hjert.k@gmail.com>
2015-04-09 23:39:48 -0700

    Update resources description data type to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150408055720) do
+ActiveRecord::Schema.define(version: 20150410063734) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 20150408055720) do
   create_table "resources", force: true do |t|
     t.string   "title"
     t.string   "url"
-    t.string   "description"
+    t.text     "description"
     t.string   "source"
     t.datetime "date"
     t.boolean  "public"
