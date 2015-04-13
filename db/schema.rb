85220b56e664b07619d8f7f46bc6fa1811b821d1
Kristina Hjertberg <hjert.k@gmail.com>
2015-04-12 17:09:47 -0700

    Default likes to zero, update existing records

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410063734) do
+ActiveRecord::Schema.define(version: 20150413000426) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20150410063734) do
     t.string   "source"
     t.datetime "date"
     t.boolean  "public"
-    t.integer  "likes"
+    t.integer  "likes",       default: 0
     t.datetime "created_at"
     t.datetime "updated_at"
   end
