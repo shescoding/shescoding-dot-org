65818b2a1e01b6c9b30fa17b9bf54358eeae2e0d
Bri Dotson <bridotson@gmail.com>
2015-04-14 07:33:40 -0700

    add jquery for advisor row formatting

--- app/assets/javascripts/team.js
+++ app/assets/javascripts/team.js
@@ -2,21 +2,29 @@ $(function() {
     var rowLength = 4;
     var $numTeamMembers = $(".team-member").size();
     var lastRow = $numTeamMembers - $numTeamMembers % rowLength;
-    console.log($numTeamMembers, lastRow);
+
     $(".team-member" ).each(function( index ) {
         index ++;
-        // console.log( index + ": " + $( this ).text() );
-        // if the index is less than lastRow, give it 12/4 in the class name
-        // if the index is >= lastRow, give it 12/($numTeamMembers % rowLength)
         if (index <= lastRow) {
-            console.log($(this));
             var numInThisRow = 12/rowLength;
-            $(this).addClass("col-md-"+numInThisRow)
-            // console.log(index, 12/4);
+            $(this).addClass("col-md-"+numInThisRow);
         } else {
             var numInThisRow = 12/($numTeamMembers % rowLength);
-            $(this).addClass("col-md-"+numInThisRow)
-            // console.log (index, 12/($numTeamMembers % rowLength))
+            $(this).addClass("col-md-"+numInThisRow);
+        }
+    });
+
+    var $numAdvisors = $(".advisor").size();
+    var lastRowAdvisors = $numAdvisors - $numAdvisors % rowLength;
+
+    $(".advisor" ).each(function( index ) {
+        index ++;
+        if (index <= lastRowAdvisors) {
+            var numInThisRow = 12/rowLength;
+            $(this).addClass("col-md-"+numInThisRow);
+        } else {
+            var numInThisRow = 12/($numAdvisors % rowLength);
+            $(this).addClass("col-md-"+numInThisRow);
         }
     });
 });
