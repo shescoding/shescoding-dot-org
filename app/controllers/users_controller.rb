b4d7c978819ba7d42afa6dd06be68ab73544c9d5
Kavita <ks@kavitasa.com>
2015-04-14 15:14:02 -0700

    whitelist user attributes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :admin)
+      params.require(:user).permit(:email, :password, :password_confirmation, :name, :admin)
     end
 end
