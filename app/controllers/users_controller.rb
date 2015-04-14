b5d2556d22a0a44332ec89c0a83172effa078dfc
Kavita <ks@kavitasa.com>
2015-04-14 15:23:17 -0700

    update without password

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,6 +41,17 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
+    if user_params[:password].blank?
+      user_params.delete(:password)
+      user_params.delete(:password_confirmation)
+    end
+
+    successfully_updated =  if needs_password?(@user, user_params)
+                              @user.update(user_params)
+                            else
+                              @user.update_without_password(user_params)
+                            end
+
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
@@ -60,6 +71,11 @@ class UsersController < ApplicationController
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  protected
+  def needs_password?(user, params)
+    params[:password].present?
   end
 
   private
