aa0bca7f692065eadbde1a0bdabde06c22596e7e
Tina Kumar <tinakumar07@gmail.com>
2015-04-15 19:23:39 -0700

    hiding for other personas

--- app/assets/javascripts/personas.js
+++ app/assets/javascripts/personas.js
@@ -9,3 +9,27 @@ $(document).ready(function() {
     $(".ui-woman-question3").toggle();
   });
 });
+
+$(document).ready(function() {
+  $(".ui-company-options").click(function() {
+    $(".ui-company-question2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".ui-company-options2").click(function() {
+    $(".ui-company-question3").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".ui-supporter-options").click(function() {
+    $(".ui-supporter-question2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".ui-supporter-options2").click(function() {
+    $(".ui-supporter-question3").toggle();
+  });
+});
