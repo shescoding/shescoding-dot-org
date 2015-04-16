147ea6e613f2c8b131d3a6360c9be89e6d9def64
Tina Kumar <tinakumar07@gmail.com>
2015-04-15 18:18:41 -0700

    hide questions

---
+++ app/assets/javascripts/personas.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".ui-woman-options").click(function() {
+    $(".ui-woman-question2").toggle();
+  });
+});
+
+$(document).ready(function() {
+  $(".ui-woman-options2").click(function() {
+    $(".ui-woman-question3").toggle();
+  });
+});
