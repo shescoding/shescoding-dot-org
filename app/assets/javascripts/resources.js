a36fd9f2725d67365625acead3dc63032d672dca
probinson2015 <porsha_robinson@ymail.com>
2016-04-28 19:37:00 -0700

    added shescoding_likes to document.cookie

--- app/assets/javascripts/resources.js
+++ app/assets/javascripts/resources.js
@@ -29,9 +29,56 @@ function scrollToTop(event) {
   $('html, body').animate({scrollTop: 0}, 'slow');
 }
 
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+
 function incrementLike(event, target) {
   event.preventDefault();
   var form = $(target).parents('form');
+
+  var resourceID = target.parentNode.parentNode.action.split('/')[4]
+  console.log(resourceID);
+
+  //no shescoding cookie object exists
+  if (getCookie("_shescoding_likes") === "")
+  {
+    document.cookie = "_shescoding_likes  = {}";
+  } 
+   
+  newValue = JSON.stringify({[resourceID]: true});
+  setCookie("_shescoding_likes", newValue, 365); 
+
+
+  // else 
+
+  //shescoding cookie object exists
+
+  
+  //check for resourceID key
+  //add shescoding resourceID and   
+
 
   var counterEl = form.find('span')[0];
   var newCount = 1 + parseInt(counterEl.innerText, 10);
@@ -46,3 +93,4 @@ function incrementLike(event, target) {
     type: "POST"
   });
 }
+
