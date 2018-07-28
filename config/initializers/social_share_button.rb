953baa820bff153a4aef82c4f42d8be200f1fb31
Porsha Robinson <porsha@Porshas-MacBook-Air.local>
2018-07-27 21:11:52 -0700

    remove pinterest sharing until no image is required to share

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -1,3 +1,3 @@
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook linkedin google_plus email pinterest)
+  config.allow_sites = %w(twitter facebook linkedin google_plus email)
 end
