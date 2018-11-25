cc73abbbb5112904e3084ce1fad78e24b3266897
Kelli Blalock <kelli.blalock2@gmail.com>
2018-11-25 01:58:58 -0800

    Update comments from coffescript to JS

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -1,5 +1,5 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
+// Place all the behaviors and hooks related to the matching controller here.
+// All this logic will automatically be available in application.js.
 
 /* Testimonial Section of front page */
 
