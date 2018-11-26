408acc9850c39f545b66d775e06697b8d2dcb271
Kelli Blalock <kelli.blalock2@gmail.com>
2018-11-26 00:03:05 -0800

    Fix testimonials not resizing in Safari

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -3,45 +3,46 @@
 
 /* Testimonial Section of front page */
 
-  // Returns height of the longest testimonial
-  function getLongestTestimonialHeight() {
-    var longestHeight = 0;
-    var height;
-    $('.item').each(function() {
-      // removes previous height added with code
-      if($(this).attr('style')) {
-        $(this).removeAttr( 'style' );
-      }
-      if ($(this).hasClass('active')) {
-        height = $(this).outerHeight();
-      } else {
-        // Bootstrap puts display: none; on carousel items not currently 
-        // shown which will have a height of 0 and temporarily cloning
-        // item is the best way to get its actual height after ruling out
-        // other options.
-        var clonedItem = $(this).clone();
-        clonedItem.css("visibility", "hidden");
-        $('.carousel-inner').append(clonedItem);
-        height = clonedItem.outerHeight();
-        clonedItem.remove();
-      }
+// Returns height of the longest testimonial
+function getLongestTestimonialHeight() {
+  var longestHeight = 0;
+  var height;
+  $('.item').each(function() {
+    // removes previous height added with code
+    if($(this).attr('style')) {
+      $(this).removeAttr( 'style' );
+    }
+    if ($(this).hasClass('active')) {
+      height = $(this).outerHeight();
+    } else {
+      // Bootstrap puts display: none; on carousel items not currently 
+      // shown which will have a height of 0 and temporarily cloning
+      // item is the best way to get its actual height after ruling out
+      // other options.
+      var clonedItem = $(this).clone();
+      clonedItem.css("visibility", "hidden");
+      $('.carousel-inner').append(clonedItem);
+      height = clonedItem.outerHeight();
+      clonedItem.remove();
+    }
 
-      if (height > longestHeight) {
-        longestHeight = height;
-      }
-    });
-    return longestHeight;
-  }
-  
-  // Set other testimonial heights to equal height of the longest one
-  function setTestimonialHeights(height) {
-    $('.item').each(function() {
-      if ($(this).height() < height) {   
-          $(this).height(height);
-      }
-    });
-  }
-  
+    if (height > longestHeight) {
+      longestHeight = height;
+    }
+  });
+  return longestHeight;
+}
+
+// Set other testimonial heights to equal height of the longest one
+function setTestimonialHeights(height) {
+  $('.item').each(function() {
+    if ($(this).height() < height) {   
+        $(this).height(height);
+    }
+  });
+}
+
+$(window).on('load', function() {
   setTestimonialHeights(getLongestTestimonialHeight());
   var resizeWindowTimer;
   // When the browser window is done being resized, recalculate longest
@@ -53,3 +54,4 @@
       setTestimonialHeights(getLongestTestimonialHeight());
     }, 250);
   });
+});
