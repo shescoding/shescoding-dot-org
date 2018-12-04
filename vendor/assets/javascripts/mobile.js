ca7d7ac8e625006a427d3b6a973825c8dae83f1b
booellean <booelle.commissions@gmail.com>
2018-12-03 21:05:19 -0800

    fix: javascript in wrong folder, placed in right folder

--- vendor/assets/javascripts/mobile.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('touchstart', addMobileClass);
-
-function addMobileClass(){
-  var allTooltipDivs = document.querySelectorAll('.the-tooltip');
-
-  allTooltipDivs.forEach( function(tooltip) {
-    tooltip.classList.add('mobile');
-  });
-}
